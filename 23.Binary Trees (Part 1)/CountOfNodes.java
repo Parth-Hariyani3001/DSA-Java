7aafba2417dc5633cf7fd7257e7195606f115be0
Parth-Hariyani3001 <hariyaniparth-inft@atharvacoe.ac.in>
2023-09-29 04:03:20 +0530

    Binary Trees Part 1

---
+++ 23.Binary Trees (Part 1)/CountOfNodes.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class CountOfNodes{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+    
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        public static int count(Node root){
+            if(root == null){
+                return 0;
+            }
+            int lc = count(root.left);
+            int rc = count(root.right);
+            return lc + rc + 1;
+        }
+    }
+    public static void main(String args[]){
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        // System.out.println(root.data);
+        // tree.preorder(root);
+        // tree.inorder(root);
+        // tree.postorder(root);
+        // tree.levelOrder(root);
+        // System.out.print(tree.height(root));
+        System.out.print(tree.count(root));
+    }
+}
+
